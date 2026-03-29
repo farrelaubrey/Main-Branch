164918399569c0f99668d9fdbcd2a7244b64003b
Jilsya <jillsyag@gmail.com>
2026-03-29 18:59:23 +0700

    Menambahkan routing halaman jillsya

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DataDiriJillsya from "./pages/DataDiriJillsya";
 
 function App() {
-  return <DataDiriJillsya />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/jillsya" element={<DataDiriJillsya />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
