69c55cc99776b9230512ce43dad2c82c396adfde
Jilsya <jillsyag@gmail.com>
2026-03-29 18:38:56 +0700

    Menambahkan halaman data diri Jillsya

---
+++ src/pages/DataDiriJillsya.jsx
@@ -0,0 +1,11 @@
+export default function DataDiriJillsya() {
+  return (
+    <div>
+      <h1>Jillsya</h1>
+      <p>Kelas: ...</p>
+      <p>Prodi: ...</p>
+      <p>Domisili: ...</p>
+      <p>Deskripsi diri minimal 100 kata...</p>
+    </div>
+  );
+}
