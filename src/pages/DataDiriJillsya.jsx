1c517b062440d32b44384fe4ff79f19b2d6b3e97
Jilsya <jillsyag@gmail.com>
2026-03-29 20:18:21 +0700

    Perbaikan tampilan profil

--- src/pages/DataDiriJillsya.jsx
+++ src/pages/DataDiriJillsya.jsx
@@ -5,7 +5,7 @@ export default function DataDiriJillsya() {
       <div className="bg-white shadow-lg rounded-xl p-8 max-w-md text-center">
 
         {/* FOTO */}
-        <div className="w-32 h-32 mx-auto rounded-full overflow-hidden mb-4">
+        <div className="w-36 h-36 mx-auto rounded-full overflow-hidden mb-4">
           <img
             src="/jillsya.jpg"
             alt="foto"
@@ -14,17 +14,18 @@ export default function DataDiriJillsya() {
         </div>
 
         {/* NAMA */}
-        <h1 className="text-2xl font-bold mb-2">
+        <h1 className="text-2xl font-bold mb-2 text-gray-800">
           Jillsya Ginta Naumira
         </h1>
 
         {/* DATA */}
-        <p>Kelas: T2C</p>
-        <p>Prodi: Teknologi Informasi</p>
-        <p>Domisili: Jawa Timur</p>
+        <p className="text-gray-700">Kelas: T2C</p>
+        <p className="text-gray-700">Prodi: Teknologi Informasi</p>
+        <p className="text-gray-700">Domisili: Jawa Timur</p>
+
 
         {/* DESKRIPSI */}
-        <p className="mt-4 text-sm text-gray-700 text-justify">
+        <p className="mt-4 text-sm text-gray-700 text-justify leading-relaxed">
           (Saya adalah seorang mahasiswa yang memiliki minat dalam bidang teknologi, khususnya pengembangan web. Saya senang mempelajari hal-hal baru dan terus berusaha meningkatkan kemampuan saya dalam pemrograman. Saat ini saya sedang belajar React dan Git untuk mendukung pengembangan aplikasi modern. Saya juga tertarik dalam desain antarmuka agar aplikasi yang dibuat tidak hanya berfungsi dengan baik tetapi juga nyaman digunakan. Ke depan, saya ingin menjadi seorang developer yang profesional dan mampu berkontribusi dalam proyek-proyek besar serta bekerja dalam tim secara efektif.)
         </p>
 
